04c1549b5cfb343658cdc5842d2791a22b5da14b
Roman <roman.camenzind@dozent.ipso.ch>
2024-11-03 11:05:09 +0100

    First Commit

---
+++ Chinook-Web-App/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Chinook_Web_App_New
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
