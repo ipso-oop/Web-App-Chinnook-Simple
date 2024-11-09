b0c3293ac316e03ae784e883f2d4b07313e5a9ee
Roman <roman.camenzind@dozent.ipso.ch>
2024-11-09 16:36:34 +0100

    Remove anti forgery argument for demo

--- Chinook-Web-App/Controllers/AlbumsController.cs
+++ Chinook-Web-App/Controllers/AlbumsController.cs
@@ -81,7 +81,6 @@ namespace Chinook_Web_App_New.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "AlbumId,Title,ArtistId")] Album album)
         {
             if (ModelState.IsValid)
